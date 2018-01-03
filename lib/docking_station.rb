68f32149d926031c8c1e24693b5c0616bd060d50
Ed Goold <teddy_goold@hotmail.com>
2018-01-03 17:34:39 +0000

    integrated tests

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,8 @@
 class DockingStation
+  #require "./lib/bike.rb"
   def release_bike
+    Bike.new
+
 
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,15 @@ require "docking_station"
 
 describe DockingStation do
   it {is_expected.to respond_to(:release_bike)}
+
+  it "releases a bike" do
+    station = DockingStation.new
+    expect(station.release_bike).to be_a Bike
+  end
+
+  it "releases working bike" do
+    station = DockingStation.new
+    bike = station.release_bike
+    expect(bike).to be_working
+  end
 end
