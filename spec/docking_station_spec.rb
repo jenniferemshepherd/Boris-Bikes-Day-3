a2de8cfb1d8cbf9aa5ede137caaba98c9f06ec37
Jennifer <jennifer.elaine.mairead@gmail.com>
2018-01-05 14:19:49 +0000

    set a default capacity if no capacity is given and adjusted other tests to reflect this

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,6 +2,18 @@ require 'docking_station'
 
 describe DockingStation do
 
+  # it {is_expected.to respond_to(:new).with(1).argument}
+
+  it {expect(DockingStation).to respond_to(:new).with(1).argument}
+  it 'sets default capacity' do
+    expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
+  end
+  it 'has a capacity of 25' do
+    station = DockingStation.new(25)
+    expect(station.capacity).to eq 25
+  end
+
+
   it {is_expected.to respond_to(:release_bike)}
   describe '#release_bike' do
     it 'releases a bike' do
@@ -22,7 +34,7 @@ describe DockingStation do
       expect {station.release_bike}.to raise_error "No bikes available"
     end
   end
-  
+
   it {is_expected.to respond_to(:dock).with(1).argument}
   describe '#dock'do
     it 'docks bike' do
@@ -33,8 +45,13 @@ describe DockingStation do
     end
 
     it 'will not dock a bike if station is full' do
-      station = DockingStation.new
-      DockingStation::DEFAULT_CAPACITY.times { station.dock Bike.new }
+      DockingStation::DEFAULT_CAPACITY.times { subject.dock Bike.new }
+      expect {subject.dock(Bike.new)}.to raise_error "Station is full"
+    end
+
+    it 'will not dock a bike if station is full' do
+      station = DockingStation.new(25)
+      station.capacity.times { station.dock Bike.new }
       expect {station.dock(Bike.new)}.to raise_error "Station is full"
     end
   end
--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -1,27 +1,27 @@
-require './lib/docking_station'
-
-def empty_station
-  DockingStation.new
-end
-
-def station_with_one_docked_bike
-    station = DockingStation.new
-    station.dock(Bike.new)
-    return station
-end
-
-def release_bike_from_empty_station
-  station = empty_station
-  station.release_bike
-end
-
-def release_bike_station_with_available
-  station = station_with_one_docked_bike
-  station.release_bike
-end
-
-def station_at_full_capacity
-  station = DockingStation.new
-  DockingStation::DEFAULT_CAPACITY.times {station.dock(Bike.new)}
-  return station
-end
+# require './lib/docking_station'
+#
+# def empty_station
+#   DockingStation.new
+# end
+#
+# def station_with_one_docked_bike
+#     station = DockingStation.new
+#     station.dock(Bike.new)
+#     return station
+# end
+#
+# def release_bike_from_empty_station
+#   station = empty_station
+#   station.release_bike
+# end
+#
+# def release_bike_station_with_available
+#   station = station_with_one_docked_bike
+#   station.release_bike
+# end
+#
+# def station_at_full_capacity
+#   station = DockingStation.new
+#   DockingStation::DEFAULT_CAPACITY.times {station.dock(Bike.new)}
+#   return station
+# end
